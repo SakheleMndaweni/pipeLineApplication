493b42d809f92a35c5be018ee10e5899b0140361
unknown <@sakhelemndaweni>
2024-04-01 20:45:23 +0200

    changed allowed hosta

--- OrderPipelineApplication/settings.py
+++ OrderPipelineApplication/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9z@$+#ec*67uat(dkggqeg&7ospcsy!$(*rm+bi*@ba-pt22me
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
